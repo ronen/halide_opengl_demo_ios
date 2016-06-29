d63f2a1aedf7a2ae7bb718271e7348f52d5ca72c
ronen barzel <ronen@barzel.org>
2016-06-29 15:21:38 -0400

    use GLKView as subview

--- PerfHarness/ViewController.m
+++ PerfHarness/ViewController.m
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Adobe Labs. All rights reserved.
 //
 
+#import <GLKit/GLKit.h>
+
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -18,6 +20,27 @@
     [super viewDidLoad];
     
     self.imageView.image = [UIImage imageNamed:@"puppy.jpg"];
+
+    EAGLContext * context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    GLKView *view = [[GLKView alloc] initWithFrame:[[UIScreen mainScreen] bounds] context:context];
+
+    // Configure renderbuffers created by the view
+    view.drawableColorFormat = GLKViewDrawableColorFormatRGBA8888;
+    view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
+    view.drawableStencilFormat = GLKViewDrawableStencilFormat8;
+    view.drawableMultisample = GLKViewDrawableMultisample4X;
+
+    view.delegate = self;
+    [self.view addSubview:view];
+
+    view.enableSetNeedsDisplay = YES;
+}
+
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
+    NSLog(@"In drawInRect");
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glFinish();
 }
 
 - (IBAction)runButtonPressed:(id)sender {
